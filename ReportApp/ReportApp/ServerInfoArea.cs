d15bcc9ff79a9b7faf58a341f68f5146868bcf23
a.chernik <a.chernik@a1qa.com>
2018-05-10 16:37:56 +0300

    First

---
+++ ReportApp/ReportApp/ServerInfoArea.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ReportApp
+{
+    class ServerInfoArea : TextBox
+    {
+        public ServerInfoArea()
+        {
+            Multiline = true;
+            Location = new Point(0, 0);
+            Name = "serverInfoArea";
+            Enabled = false;
+            Dock = DockStyle.Fill;
+        }
+    }
+}
---
+++ ReportApp/ReportApp/ServerMainForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReportApp
+{
+    public partial class ServerMainForm : Form
+    {
+        private const int ConnectPort = 8888;
+        private readonly TcpListener _listener;
+        private int _connectedClients;
+
+        public ServerMainForm()
+        {
+            InitializeComponent();
+            _listener = new TcpListener(IPAddress.Any, ConnectPort);
+            _listener.Start();
+            serverInfoArea.Text += $"Server started on {ConnectPort} port{Environment.NewLine}";
+
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    new ClientThread(_listener.AcceptTcpClient());
+                    serverInfoArea.Text += $"{_connectedClients++} client connected{Environment.NewLine}";
+                }
+            });
+        }
+
+        public void AddText(string text) => serverInfoArea.Text += text;
+
+        ~ServerMainForm() => _listener?.Stop();
+    }
+
+
+
+}
